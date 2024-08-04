539151dfb8b2e7ffbb4c4cc960b0deac678fe303
Ryan Long <rlong@wasabi.com>
2024-08-03 21:39:47 -0400

    Adding gitignore and a main.py file

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def fetchWeather(lat, lon):
+    response =  requests.get(f"https://api.weather.gov/points/{lat},{lon}")  .json()
+    x = response["@context"][1]["@version"]
+    return x
+
+
+if __name__ == "__main__":
+    print(fetchWeather('41.409','-75.6624'))
+
+
